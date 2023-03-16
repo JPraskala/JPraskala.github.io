042e523f097ed5175c8c2a1d9c4150e15b8d76fc
JPraskala <92476742+JPraskala@users.noreply.github.com>
2023-03-15 21:41:17 -0400

    Updated the Accessibility Site

--- itis3135/Exercise/Praskala-Activity-Accessibility.js
+++ itis3135/Exercise/Praskala-Activity-Accessibility.js
@@ -1,16 +1,22 @@
 document.getElementById('submit-button').addEventListener('click', function() 
 {
     const form = document.querySelector(".access-form")
-    const name = document.getElementById("name").value 
-    const number = document.getElementById('phone-number').value
-    if (name != '' && number != '') 
+    const name = document.getElementById("name")
+    const number = document.getElementById('phone-number')
+    const numberExpression = /^\d{3}\d{3}\d{4}|^\d{3}[-]\d{3}[-]\d{4}/
+    if (name.value != '' && number.value.match(numberExpression)) 
     {
-        const result = document.createElement('p')
-        result.textContent = `Congratulations ${name}, you have won an Xbox Series X!!!`
-        form.appendChild(result)
+        if (document.getElementById('message') == null) 
+        {
+            const result = document.createElement('p')
+            result.setAttribute('id', 'message')
+            result.style.color = 'orange'
+            result.textContent = `Congratulations ${name.value}, you have won an Xbox Series X 2023 edition!!!`
+            form.insertAdjacentElement('afterend', result)
+        }
     }
     else 
     {
-        alert("Please enter a name and a phone number")
+        alert("Error 400 bad request. Please try again.")
     }
 })
